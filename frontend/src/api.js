f3086d598333b09e1aa696adb400a31dde4a0f5a
nanditapanchal <nanditapanchalg@gmail.com>
2025-10-27 12:44:08 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
+
 const API = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL || 'http://localhost:5000/api',
+  baseURL: import.meta.env.VITE_BACKEND_URL || 'http://localhost:5000/api',
 });
 
 API.interceptors.request.use(config => {
